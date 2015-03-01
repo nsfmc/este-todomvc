d9fe167561f5c74132cf0b4d4a0323e865516c4d
Daniel Steigerwald <daniel@steigerwald.cz>
2015-03-01 04:52:36 +0100

    Hmm.

--- src/client/dispatcher.js
+++ src/client/dispatcher.js
@@ -2,7 +2,7 @@ import flux from 'flux'
 
 export default new class Dispatcher extends flux.Dispatcher {
 
-  dispatch(action: Function, data?: Object) {
+  dispatch(action: Function, data: Object) {
     super.dispatch({action, data})
   }
 
